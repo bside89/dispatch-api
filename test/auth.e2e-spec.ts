8b71bd602b5a205901db56a32d7638533ec5ab71
Bruno Santos <bside89@live.com>
2026-04-08 02:58:39 -0300

    test: add new e2e test for Auth endpoints

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,139 @@
+import { AppModule } from '@/app.module';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { ADMIN_USER } from './constants/admin-user.constant';
+import { cleanDatabase, cleanRedis } from './utils/database-cleaner';
+import { DataSource } from 'typeorm';
+import Redis from 'ioredis';
+import { REDIS_CLIENT } from '@/shared/constants/redis-client.constant';
+
+describe('Auth (E2E)', () => {
+  let app: INestApplication;
+  let dataSource: DataSource;
+  let redisClient: Redis;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = module.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+
+    await app.init();
+
+    dataSource = app.get<DataSource>(DataSource);
+    redisClient = app.get<Redis>(REDIS_CLIENT);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    await cleanDatabase(dataSource);
+    await cleanRedis(redisClient);
+  });
+
+  describe('REST Endpoints', () => {
+    it('POST /v1/auth/login - should deny access with incorrect email/password', async () => {
+      await request(app.getHttpServer())
+        .post('/v1/auth/login')
+        .send({
+          email: 'wrong@test.com',
+          password: 'WrongPassword123!',
+        })
+        .expect(HttpStatus.UNAUTHORIZED);
+    });
+
+    it('POST /v1/auth/login - should authenticate Admin user and return tokens', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/v1/auth/login')
+        .send({
+          email: ADMIN_USER.email,
+          password: ADMIN_USER.password,
+        })
+        .expect(HttpStatus.CREATED);
+
+      expect(res.body.data).toHaveProperty('accessToken');
+      expect(res.body.data).toHaveProperty('refreshToken');
+    });
+
+    it('POST /v1/auth/refresh - should deny access with incorrect refreshToken', async () => {
+      await request(app.getHttpServer())
+        .post('/v1/auth/refresh')
+        .set('Authorization', `Bearer invalid-token`)
+        .expect(HttpStatus.UNAUTHORIZED);
+    });
+
+    it('POST /v1/auth/refresh - should return new accessToken and refreshToken', async () => {
+      // First, login to get a valid refresh token
+      const loginRes = await request(app.getHttpServer())
+        .post('/v1/auth/login')
+        .send({
+          email: ADMIN_USER.email,
+          password: ADMIN_USER.password,
+        })
+        .expect(HttpStatus.CREATED);
+
+      const validRefreshToken = loginRes.body.data.refreshToken;
+
+      const refreshRes = await request(app.getHttpServer())
+        .post('/v1/auth/refresh')
+        .set('Authorization', `Bearer ${validRefreshToken}`)
+        .expect(HttpStatus.CREATED);
+
+      expect(refreshRes.body.data).toHaveProperty('accessToken');
+      expect(refreshRes.body.data).toHaveProperty('refreshToken');
+      expect(refreshRes.body.data.accessToken).not.toBe(
+        loginRes.body.data.accessToken,
+      );
+    });
+
+    it('POST /v1/auth/logout - should deny access with incorrect accessToken', async () => {
+      await request(app.getHttpServer())
+        .post('/v1/auth/logout')
+        .set('Authorization', `Bearer invalid-token`)
+        .expect(HttpStatus.UNAUTHORIZED);
+    });
+
+    it('POST /v1/auth/logout - should authenticate Admin and successfully logout, blacklisting token', async () => {
+      // 1. Login to get a valid access token
+      const loginRes = await request(app.getHttpServer())
+        .post('/v1/auth/login')
+        .send({
+          email: ADMIN_USER.email,
+          password: ADMIN_USER.password,
+        })
+        .expect(HttpStatus.CREATED);
+
+      const validAccessToken = loginRes.body.data.accessToken;
+
+      // 2. Perform logout
+      await request(app.getHttpServer())
+        .post('/v1/auth/logout')
+        .set('Authorization', `Bearer ${validAccessToken}`)
+        .expect(HttpStatus.CREATED);
+
+      // 3. Try another authenticated endpoint with the blacklisted token
+      await request(app.getHttpServer())
+        .post('/v1/auth/refresh')
+        .set('Authorization', `Bearer ${validAccessToken}`)
+        .expect(HttpStatus.UNAUTHORIZED);
+
+      // We can also test GET /v1/users to see if logout really worked
+      await request(app.getHttpServer())
+        .get('/v1/users')
+        .set('Authorization', `Bearer ${validAccessToken}`)
+        .expect(HttpStatus.UNAUTHORIZED);
+    });
+  });
+});
