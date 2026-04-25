9b896e6677d0eed69dc4ae75230f2a4f18b70098
Bruno Santos <bside89@live.com>
2026-04-24 22:10:08 -0300

    refactor: change throttle.config to a better format

--- src/config/throttle.config.ts
+++ src/config/throttle.config.ts
@@ -1,20 +1,32 @@
 import { ConfigService } from '@nestjs/config';
+import { ThrottlerModuleOptions } from '@nestjs/throttler';
 
-const DEFAULT_THROTTLE_LIMIT = 60;
-export const TEST_THROTTLE_LIMIT = 999999;
+const DEFAULT_THROTTLE_GLOBAL_TTL = 60 * 1000; // 1 minute
+const DEFAULT_THROTTLE_GLOBAL_LIMIT = 60;
+const DEFAULT_THROTTLE_LIMIT = 10;
+const TEST_THROTTLE_LIMIT = 10000; // High limit for tests to avoid throttling
 
-export const resolveThrottleLimit = (limit: number) =>
-  process.env.TEST_ENV === 'true' ? TEST_THROTTLE_LIMIT : limit;
+export const resolveThrottleLimit = (limit?: number): number => {
+  if (process.env.TEST_ENV === 'true') {
+    return TEST_THROTTLE_LIMIT;
+  }
+  if (limit && limit > 0) {
+    return limit;
+  }
+  return DEFAULT_THROTTLE_LIMIT;
+};
 
-export const throttleConfig = (configService: ConfigService) => ({
+export const throttleConfig = (
+  configService: ConfigService,
+): ThrottlerModuleOptions => ({
   throttlers: [
     {
-      ttl: 60 * 1000, // 1 minute
-      // If TEST_ENV is true, set a very high limit to effectively disable throttling during tests.
-      limit:
-        configService.get('TEST_ENV') === 'true'
-          ? TEST_THROTTLE_LIMIT
-          : DEFAULT_THROTTLE_LIMIT,
+      ttl: DEFAULT_THROTTLE_GLOBAL_TTL,
+      limit: DEFAULT_THROTTLE_GLOBAL_LIMIT,
     },
   ],
+  skipIf: () => {
+    // Skip throttling for tests
+    return configService.get('TEST_ENV') === 'true';
+  },
 });
